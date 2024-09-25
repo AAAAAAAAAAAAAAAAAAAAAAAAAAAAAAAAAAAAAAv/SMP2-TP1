bccb18dcd5007f169aa68daf04db80250a593811
Guillaume-ctrl <ps3aknew@gmail.com>
2024-09-25 14:24:08 +0200

    Ajout de fichier pour le début du dev

---
+++ Lexique.h
@@ -0,0 +1,16 @@
+#ifndef LEXIQUE_H
+#define LEXIQUE_H
+
+#include <string>
+#include <stdint>
+
+class Lexique {
+    
+    
+    
+protected:
+    std::string _nom;
+    uint32_t _occurence;
+};
+
+#endif //LExIQUE_H
