5e4d4d6e5e07b50b4c0df4af360db309867e616a
Louis <Louis.Pean@eleves.ec-nantes.fr>
2024-09-25 15:53:18 +0200

    added lexique function
    should work? I cant seem to run it , but it compiles so it should be fine

--- Lexique.cpp
+++ Lexique.cpp
@@ -1,2 +1,16 @@
 #include "Lexique.h"
-
+#include "utilitaire.hpp"
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+Lexique::Lexique(const std::string& fileName)
+{
+    readFileIntoString(fileName, this->inputString);
+    char* cleanedString = strtok((char*)inputString.c_str(), " ,.;-");
+    while (cleanedString != NULL)
+    {
+        std::string currentWord = cleanedString;
+        this->occurences[currentWord]++;
+        cleanedString = strtok(NULL, " ,.;-\"\r\n\t");
+    };
+}
--- Lexique.h
+++ Lexique.h
@@ -4,18 +4,22 @@
 #include <string>
 #include <stdint.h>
 #include <vector>
+#include <map>
 
 class Lexique {
 public:
     Lexique() = default;
-    explicit Lexique(std::string nom);
+    explicit Lexique(const std::string& fileName);
     explicit Lexique(Lexique const& l);
     virtual ~Lexique() = default;
     
 protected:
     std::string _nom;
-    uint32_t _occurence;
+    //uint32_t _occurence;
     std::vector<std::string> _lexique;
+private:
+	std::map<std::string, unsigned int> occurences;
+	std::string inputString = "";
 };
 
 #endif //LEXIQUE_H
