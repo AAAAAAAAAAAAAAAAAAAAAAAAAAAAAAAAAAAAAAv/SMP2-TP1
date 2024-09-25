2acd622ed8e16dbddd8fa79c4ed4884754097ab9
Guillaume-ctrl <ps3aknew@gmail.com>
2024-09-25 14:35:04 +0200

    Ajout d'un fichier de sortie pour le Lexique

--- Lexique.h
+++ Lexique.h
@@ -3,14 +3,15 @@
 
 #include <string>
 #include <stdint>
+#include <vector>
 
 class Lexique {
-    
     
     
 protected:
     std::string _nom;
     uint32_t _occurence;
+    std::vector<string> _lexique;
 };
 
 #endif //LExIQUE_H
